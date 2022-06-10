8070ee50170f1dcacc5b44f2e0004e60c639a856
Vardan Melkonyan <vardan.melkonyan@tumo.org>
2022-06-10 18:51:14 +0400

    LIB-1 create the project

---
+++ lib/pages/home.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:get/get.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Center(child: Text("data"))
+        ],
+      ),
+    );
+  }
+}
